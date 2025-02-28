ad1fcb314b1d6d351832b0cfe717156d76c8d6bb
namansingla-coder <namansingla@myyahoo.com>
2025-02-28 11:26:19 +0530

    component floder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import HomePage from './HomePage';
-import ChatApp from './ChatApp';
+import HomePage from './components/HomePage';
+import ChatApp from './components/ChatApp';
 function App() {
   const [count, setCount] = useState(0)
 
